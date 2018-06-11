05ccb0e35995c2216e3d0afbdacc861c6fd9bf4e
Sandro Meier <sandro.meier@fidelisfactory.ch>
2018-06-11 22:15:54 +0200

    Add first alexa response

--- actions/led.py
+++ actions/led.py
@@ -1,10 +1,31 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from decorators import validate_api_key
 
 led_page = Blueprint('led_page', __name__)
 
 
-@led_page.route('/on')
+@led_page.route('/on', methods=['POST', 'GET'])
 @validate_api_key
 def turn_on():
-    return "success", 200
+    response = {
+        "version": "1.0",
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"
+            },
+            "card": {
+                "type": "Simple",
+                "title": "Horoscope",
+                "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+            },
+            "reprompt": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Can I help you with anything else?"
+                }
+            },
+            "shouldEndSession": False
+        }
+    }
+    return jsonify(response)
