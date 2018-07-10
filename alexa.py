2d9949c3a7a77f9bdfb9075fe005a83a3f5da7ba
Sandro Meier <sandro.meier@fidelisfactory.ch>
2018-07-10 17:22:25 +0200

    Fix

--- alexa.py
+++ alexa.py
@@ -13,7 +13,7 @@ class Reqeust(object):
 
     def is_turn_off_request(self):
         intent_name = self._get_intent_name()
-        return intent_name == "TurnOff" or "AMAZON.StopIntent"
+        return intent_name == "TurnOff" or intent_name == "AMAZON.StopIntent"
 
     def is_play_request(self):
         intent_name = self._get_intent_name()
