1c7486eca14d1b7824d6371160c36fcb3f359edc
Sandro Meier <sandro.meier@fidelisfactory.ch>
2018-07-15 11:26:11 +0200

    Restructure communication to only open serial when communication is happening.

--- hardware/communication.py
+++ hardware/communication.py
@@ -1,11 +1,5 @@
 from serial import Serial
 
-# Initialize the serial
-_serial = Serial(
-    "/dev/ttyUSB0",
-    9600,
-    timeout=1.0
-)
 
 class Command:
     """
@@ -29,8 +23,7 @@ def check_connectivity(self):
     Check if the Arduino is connected and working
     :return: True if the arduino is connected and running as expected.
     """
-    self._send_command(Command.STATUS)
-    return self._check_ok()
+    return self._send_command(Command.STATUS)
 
 
 def play_pattern(pattern):
@@ -39,8 +32,7 @@ def play_pattern(pattern):
     :param pattern: The identifier of the pattern.
     :return: True if the pattern was started, false otherwise.
     """
-    _send_command(Command.PLAY, pattern)
-    return _check_ok()
+    return _send_command(Command.PLAY, pattern)
 
 
 def turn_off():
@@ -48,8 +40,7 @@ def turn_off():
     Turn off the current light
     :return:
     """
-    _send_command(Command.STOP)
-    return _check_ok()
+    return _send_command(Command.STOP)
 
 
 def _send_command(command, parameter=0):
@@ -57,16 +48,24 @@ def _send_command(command, parameter=0):
     Send a command including the parameter to the arduino.
     :param command: The command to send. An integer.
     :param parameter: The parameter to send with the command.
+    :return True when the arduino responded with OK. False otherwise.
     """
+
+    # Open the serial connection
+    _serial = Serial(
+        "/dev/ttyUSB0",
+        9600,
+        timeout=1.0
+    )
+
     _serial.write(bytes([command, parameter]))
 
-
-def _check_ok():
-    """
-    Check if the Arduino responded with an OK.
-    :return:
-    """
     data = _serial.read()
-    if len(data) != 0:
-        return False
-    return data[0] == Response.OK
+    if len(data) == 0:
+        success = False
+    else:
+        success = data[0] == Response.OK
+
+    _serial.close()
+    return success
+
